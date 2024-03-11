48df1b9f1b5747ce81619306a7706b6304fabf0d
KNU-K <knukang334@gmail.com>
2024-03-11 22:26:07 +0900

    #5 [feat/#5-auth] - auth controller 구현
    로그인 기능만, 로그아웃이나 나머지 기능은 추후 고도화 예정

---
+++ server1/controllers/auth.controller.js
@@ -0,0 +1,34 @@
+const { accessToken, refreshToken } = require("../config/token.config");
+const userService = require("../services/user.service");
+
+class AuthController {
+  async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await userService.readOneByEmail(email);
+      if (user.password !== password) new Error("not matched user password");
+
+      const payload = {
+        username: user.username,
+        email: user.email,
+        region: user.region,
+        tel: user.tel,
+      };
+      const issuedAccessToken = await accessToken.generateToken(payload, "30m");
+      const issuedRefreshToken = await refreshToken.generateToken(
+        payload,
+        "90h"
+      );
+
+      res.status(201).json({
+        accessToken: issuedAccessToken,
+        refreshToken: issuedRefreshToken,
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+module.exports = new AuthController();
