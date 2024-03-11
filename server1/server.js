6975203f04dc87c58997a7a17ab6508bd75929a3
KNU-K <knukang334@gmail.com>
2024-03-11 15:59:16 +0900

    #5 [feat/#5-user] - router 설정 및 json 파서 미들웨어 적용
    /api/* 주소로 api 지정되게 했음.

--- server1/server.js
+++ server1/server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const { connection } = require("./config/db.config");
 const app = express();
 const port = process.env.SERVER_PORT || 8000;
+const apiIndex = require("./api/index");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/**@ROUTER router render point */
+app.use("/api", apiIndex);
+
+/**@TEST DB connection TEST */
 connection.connect((err) => {
   if (err) {
     throw err;
