9b7d633318caf9937bd9dc291ae7c8a81423da9c
KNU-K <knukang334@gmail.com>
2024-03-11 22:26:28 +0900

    #5 [feat/#5-auth] - router 설정 (auth)

--- server1/api/index.js
+++ server1/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const user = require("./user/index");
 const question = require("./question/index");
+const auth = require("./auth/index");
 router.use("/user", user);
 router.use("/question", question);
+router.use("/auth", auth);
 module.exports = router;
