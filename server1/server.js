c0ea41a5a893973abd20cdded3c6fc1a418a414a
KNU-K <knukang334@gmail.com>
2024-03-11 14:09:13 +0900

    #5 - connection test - 결과 완료
    성공적으로 AWS의 RDS와 연결 완료

--- server1/server.js
+++ server1/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const { connection } = require("./config/db.config");
 const app = express();
 const port = process.env.SERVER_PORT || 8000;
-
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("successful");
+  }
+});
 app.listen(port, () => {
   console.log("server open!");
 });
