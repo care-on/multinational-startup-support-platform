dfc224a7e70d8dcafaac8c31230d7ecc078d67a0
KNU-K <knukang334@gmail.com>
2024-03-11 23:06:44 +0900

    #5 [feat/#5-swagger] - json 형식의 명세 데이터를 swagger로 뿌려주기
    프론트 개발에 가시성을 돋기 위해서

--- server1/server.js
+++ server1/server.js
@@ -4,7 +4,8 @@ const app = express();
 const port = process.env.SERVER_PORT || 8000;
 const apiIndex = require("./api/index");
 const errorMiddleware = require("./middlewares/error.middleware");
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -13,6 +14,7 @@ app.use("/api", apiIndex);
 
 app.use("/api", errorMiddleware);
 
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 /**@TEST DB connection TEST */
 connection.connect((err) => {
   if (err) {
