24ee4f6d5c736f21d7365938de4764edda88e743
KNU-K <knukang334@gmail.com>
2024-03-11 22:49:52 +0900

    #5 [feat/#5-middleware] - error 기저 미들웨어 설정
    세부적인 셋팅없이 서버 안꺼지게..

--- server1/server.js
+++ server1/server.js
@@ -3,12 +3,15 @@ const { connection } = require("./config/db.config");
 const app = express();
 const port = process.env.SERVER_PORT || 8000;
 const apiIndex = require("./api/index");
+const errorMiddleware = require("./middlewares/error.middleware");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 /**@ROUTER router render point */
 app.use("/api", apiIndex);
+
+app.use("/api", errorMiddleware);
 
 /**@TEST DB connection TEST */
 connection.connect((err) => {
