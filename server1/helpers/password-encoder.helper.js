94a629cba2aa8fb326484fd53916954becb9e75d
KNU-K <knukang334@gmail.com>
2024-03-16 01:59:58 +0900

    #8 [feat/#8-user] - password 암호화 라이브러리 제작
    password 암호화 라이브러리 제작

---
+++ server1/helpers/password-encoder.helper.js
@@ -0,0 +1,32 @@
+const crypto = require("bcrypt");
+class PasswordEncoder {
+  constructor(saltRounds) {
+    this.saltRound = saltRounds;
+    this.initialize();
+  }
+
+  async initialize() {
+    try {
+      this.salt = await crypto.genSaltSync(saltRounds);
+    } catch (err) {
+      throw err;
+    }
+  }
+  async encode(plainText) {
+    try {
+      return crypto.hashSync(plainText, this.salt);
+    } catch (err) {
+      throw err;
+    }
+  }
+  async compare(plainText, encodedText) {
+    try {
+      const isSame = await crypto.compareSync(plainText, encodedText);
+      return isSame;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+module.exports = PasswordEncoder;
