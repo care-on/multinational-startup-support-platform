9dd5a1179c8ad60f4ace47b0aec9cfe308410931
KNU-K <knukang334@gmail.com>
2024-03-16 19:36:57 +0900

    #8 [feat/#8-post] - 계층형 구조 접목으로 question service에서 answer 기능까지 지원.
    지속 부하 감소,, 초반 리소스 소모를 하는 것을 ( trade-off함)

--- server1/services/question.service.js
+++ server1/services/question.service.js
@@ -4,10 +4,9 @@ const { question } = require("../models/question.model");
 class QuestionService {
   constructor() {
     this.questions = [];
-    this.initializeQuestions();
+    this.initializeQuestionsAndAnswers();
   }
-
-  async initializeQuestions() {
+  async initializeQuestionsAndAnswers() {
     try {
       const [rows, fields] = await connection_pool.query(
         "call GetQuestionDetails()"
@@ -43,6 +42,24 @@ class QuestionService {
     }
   }
 
+  async createAnswer(newAnswer) {
+    try {
+      const [result, fields] = await connection_pool.query(
+        "call InsertAnswerAndRelation(?,?,?)",
+        [newAnswer.uid, newAnswer.qid, newAnswer.content]
+      );
+      this.questions.forEach((question) => {
+        if (question.qid === newAnswer.qid) question.child.push(result[0][0]);
+      });
+
+      //this.questions.push(new question.hierarchy({ ...rows[0][0], child: [] }));
+      //   return rows[0][0];
+      return result[0][0];
+    } catch (err) {
+      throw err;
+    }
+  }
+
   async read() {
     try {
       return this.questions;
@@ -67,6 +84,17 @@ class QuestionService {
       const question = this.questions.find((question) => question.qid === qid);
       if (!question) throw new Error("cannot find question post");
       return question.uid;
+    } catch (err) {
+      throw err;
+    }
+  }
+  async findAnswerAuthor(qid, aid) {
+    try {
+      const question = this.questions.find((question) => question.qid === qid);
+      console.log(aid);
+      const answer = question.child.find((answer) => answer.aid === aid);
+      if (!answer) throw new Error("cannot find question post");
+      return answer.uid;
     } catch (err) {
       throw err;
     }
@@ -96,6 +124,35 @@ class QuestionService {
     }
   }
 
+  async updateAnswer(aid, updatedAnswer) {
+    try {
+      await connection_pool.query(
+        "UPDATE answers SET ? WHERE aid = ? and uid = ?",
+        [updatedAnswer, aid, updatedAnswer.uid]
+      );
+
+      // 업데이트된 질문을 다시 가져와서 업데이트
+      const [rows, fields] = await connection_pool.query(
+        "call GetAnswerDetailsById(?)",
+        [aid]
+      );
+
+      const updatedIndex = this.questions.findIndex(
+        (q) => q.qid === updatedAnswer.qid
+      );
+      if (updatedIndex === -1) throw new Error("cannot found post");
+      const childIndex = this.questions[questionIndex].child.findIndex(
+        (answer) => updatedAnswer.aid === aid
+      );
+
+      if (childIndex === -1) throw new Error("cannot found answer");
+      this.questions[updatedIndex].child[childIndex] = rows[0];
+      return rows[0];
+    } catch (err) {
+      throw err;
+    }
+  }
+
   async delete(qid) {
     try {
       const result = await connection_pool.query(
@@ -113,6 +170,31 @@ class QuestionService {
         return deletedQuestion;
       } else {
         throw new Error("not found question.");
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
+  async deleteAnswer(qid, aid) {
+    try {
+      const result = await connection_pool.query(
+        "UPDATE answers SET deleted_date = CURRENT_TIMESTAMP WHERE aid = ?",
+        [aid]
+      );
+
+      if (result[0].affectedRows > 0) {
+        const questionIndex = this.questions.findIndex(
+          (question) => question.qid === qid
+        );
+        if (questionIndex !== -1) {
+          this.questions[questionIndex].child = this.questions[
+            questionIndex
+          ].child.filter((answer) => answer.aid !== aid);
+        }
+
+        return result[0];
+      } else {
+        throw new Error("not found answer.");
       }
     } catch (err) {
       throw err;
