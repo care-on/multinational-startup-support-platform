2d75c5927ae7b2825991a94343234b72a3559998
KNU-K <knukang334@gmail.com>
2024-03-16 17:03:45 +0900

    #8 [feat/#8-post] - 조회 시 좋아요, 조회수 procedure 단위로 불러오는 방식으로 변경
    procedure를 가용해서 트랜잭션 단위로 제어하게 변경 이는 sql 문을 hide하며, 좀 더 간편하게 제어할 수 있다.

--- server1/api/question/index.js
+++ server1/api/question/index.js
@@ -1,3 +1,4 @@
+const answerController = require("../../controllers/answer.controller");
 const questionController = require("../../controllers/question.controller");
 const guardMiddleware = require("../../middlewares/guard.middleware");
 
@@ -6,6 +7,15 @@ const router = require("express").Router();
 /**CRUD QUESTION */
 router.post("/", guardMiddleware, questionController.create);
 router.get("/", questionController.read);
+router.get("/:qid", questionController.readOneByQid);
 router.put("/", guardMiddleware, questionController.update);
 router.delete("/", guardMiddleware, questionController.delete);
+
+/**CRUD ANSWER */
+router.get("/:qid/answer", answerController.read);
+router.get("/:qid/answer/:aid");
+router.post("/:qid/answer", answerController.create);
+router.put("/:qid/answer/:aid", answerController.update);
+router.delete("/:qid/answer/:aid", answerController.delete);
+
 module.exports = router;
