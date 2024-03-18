78f5c3144545493ce72269099af41b19130935c0
KNU-K <knukang334@gmail.com>
2024-03-18 20:23:15 +0900

    #8 [hofix] - 트랜잭션 수정

--- server1/sql/procedures/GetAnswersByQuestionId.sql
+++ server1/sql/procedures/GetAnswersByQuestionId.sql
@@ -1,11 +1,26 @@
-
 DELIMITER //
 
 CREATE PROCEDURE GetAnswersByQuestionId(IN question_id INT)
 BEGIN
-    SELECT a.*
+    SELECT a.aid, 
+           a.uid, 
+           a.content, 
+           a.created_date, 
+           a.updated_date,
+           COALESCE(h.hit_count, 0) AS hit_count,
+           COALESCE(l.likes, 0) AS like_count
     FROM answers a
     JOIN questions_answers qa ON a.aid = qa.aid
+    LEFT JOIN (
+        SELECT aid, COUNT(*) AS hit_count
+        FROM answer_hit_log
+        GROUP BY aid
+    ) h ON a.aid = h.aid
+    LEFT JOIN (
+        SELECT aid, COUNT(*) AS likes
+        FROM answer_like_log
+        GROUP BY aid
+    ) l ON a.aid = l.aid
     WHERE qa.qid = question_id
     AND a.deleted_date IS NULL;
 END//
