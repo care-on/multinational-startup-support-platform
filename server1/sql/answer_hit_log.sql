f3d300ddd47235d8b3870278e9bf1d1824030d79
KNU-K <knukang334@gmail.com>
2024-03-19 15:02:07 +0900

    #8 [#8-post] answer에는 굳이 조회수가 필요없어서 삭제

--- server1/sql/answer_hit_log.sql
+++
@@ -1,8 +0,0 @@
-create table answer_hit_log(
-	id int auto_increment primary key,
-    aid int not null,
-    uid int not null,
-    hit int not null,
-    foreign key (aid) references answers(aid),
-    foreign key (uid) references users(uid)
-);
