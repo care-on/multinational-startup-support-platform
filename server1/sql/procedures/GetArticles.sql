8f21dc395f639333efe9136a370dddd9e0f3c155
KNU-K <knukang334@gmail.com>
2024-03-21 16:48:16 +0900

    #8 [#8-post]  article -  고도화를 위한 프로시저 추가

--- server1/sql/procedures/GetArticles.sql
+++ server1/sql/procedures/GetArticles.sql
@@ -1,28 +1,35 @@
-DELIMITER //
-CREATE PROCEDURE GetArticles()
+CREATE PROCEDURE GetArticles(IN pageNumber INT, IN pageSize INT)
 BEGIN
-SELECT a.a_id, 
-	   a.a_title, 
-	   a.date_begin, 
-	   a.date_end, 
-	   a.agency, 
-	   a.tag,
-	   a.organization, 
-	   COALESCE(h.hit_count, 0) AS hit_count,
-	   COALESCE(l.like_count, 0) AS like_count
-FROM articles a
-LEFT JOIN (
-	SELECT a_id, COUNT(*) AS hit_count
-	FROM article_hit_log
-	GROUP BY a_id
-) h ON a.a_id = h.a_id
-LEFT JOIN (
-	SELECT a_id, COUNT(*) AS like_count
-	FROM article_like_log
-	GROUP BY a_id
-) l ON a.a_id = l.a_id
-where a_id = 168233;
+   DECLARE totalRecords INT;
+    DECLARE totalPages INT;
+    DECLARE offsetValue INT;
+    SET offsetValue = (pageNumber - 1) * pageSize;
+
+    -- 전체 항목의 개수 계산
+    SELECT COUNT(*) INTO totalRecords FROM articles;
+
+    -- 페이지네이션된 데이터 반환
+    SELECT a.a_id, 
+           a.a_title, 
+           a.date_begin, 
+           a.date_end, 
+           a.agency, 
+           a.tag,
+           a.organization, 
+           COALESCE(h.hit_count, 0) AS hit_count,
+           COALESCE(l.like_count, 0) AS like_count
+    FROM articles a
+    LEFT JOIN (
+        SELECT a_id, COUNT(*) AS hit_count
+        FROM article_hit_log
+        GROUP BY a_id
+    ) h ON a.a_id = h.a_id
+    LEFT JOIN (
+        SELECT a_id, COUNT(*) AS like_count
+        FROM article_like_log
+        GROUP BY a_id
+    ) l ON a.a_id = l.a_id
+    LIMIT offsetValue, pageSize;
 END//
 
 DELIMITER ;
-
